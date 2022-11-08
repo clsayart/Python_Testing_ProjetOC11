3ae05946a2576dc525879b81455ff33ce3169950
SAYART Claire <csayart@twm.ch>
2022-11-08 11:00:36 +0100

    bug full competition

--- server.py
+++ server.py
@@ -51,9 +51,8 @@ def purchasePlaces():
     competition = [c for c in competitions if c['name'] == request.form['competition']][0]
     club = [c for c in clubs if c['name'] == request.form['club']][0]
     placesRequired = int(request.form['places'])
-    competition['numberOfPlaces'] = int(competition['numberOfPlaces']) - placesRequired
-    if placesRequired > int(club['points']):
-        flash('Error-you do not have enough points to complete this purchase')
+    if placesRequired > int(competition['numberOfPlaces']):
+        flash('Error-there are not enough available places to complete this purchase')
     else:
         competition['numberOfPlaces'] = int(competition['numberOfPlaces']) - placesRequired
         flash('Great-booking complete!')
---
+++ tests/unit_tests/test_full_competition.py
@@ -0,0 +1,25 @@
+import sys
+
+sys.path.append('C:\\Users\\csayart\\PycharmProjects\\pythonProject11\\Python_Testing_ProjetOC11\\tests')
+import fixtures
+
+mock_load = fixtures.mock_loadCompetitions_and_loadClubs
+
+
+def test_purchase_available_places(client, mock_load):
+    response = client.post('/purchasePlaces', data={'club': 'Simply Lift', 'competition': 'Fall Classic',
+                                                    'places': '13'})
+    response_data = response.data.decode()
+    assert response.status_code == 200
+    assert 'Great-booking complete!' in response_data
+
+
+def test_purchase_more_than_available_places(client, mock_load):
+    response = client.post('/purchasePlaces', data={'club': 'Simply Lift', 'competition': 'Fall Classic',
+                                                    'places': '14'})
+    response_data = response.data.decode()
+    assert response.status_code == 200
+    assert 'Error-there are not enough available places' in response_data
+
+
+
