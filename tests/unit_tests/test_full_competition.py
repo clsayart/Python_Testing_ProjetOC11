081e62b60ffafe52ef0e571d18ed3d5de667a932
SAYART Claire <csayart@twm.ch>
2022-11-15 12:26:50 +0100

    update tests

--- tests/unit_tests/test_full_competition.py
+++ tests/unit_tests/test_full_competition.py
@@ -8,7 +8,7 @@ mock_load = fixtures.mock_loadCompetitions_and_loadClubs
 
 def test_purchase_available_places(client, mock_load):
     response = client.post('/purchasePlaces', data={'club': 'Simply Lift', 'competition': 'Fall Classic',
-                                                    'places': '13'})
+                                                    'places': '12'})
     response_data = response.data.decode()
     assert response.status_code == 200
     assert 'Great-booking complete!' in response_data
